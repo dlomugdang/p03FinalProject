ac7d15115e7349ef259c05c5617460b8fad01a27
Unknown <dlomugdang@mail.csuchico.edu>
2017-05-03 10:03:23 -0700

    Start

---
+++ cartoon.js
@@ -0,0 +1,12 @@
+/* Lewis Lomugdang */
+var cartoon = [
+  { description: "Yoda",
+    picture: "https://image.desiringgod.org/be-a-yoda-not-a-grinch-en/legacy_landscape/large_be-a-yoda-not-a-grinch.jpg?0",
+    from: "desiringgod.org"
+  };
+]
+
+function shufflePics()
+{
+
+}
